6972f1967e682ad58a1702d488565779c5b0ccbe
Catalin Boruga <catalinboruga@yahoo.com>
2015-07-10 21:49:21 +0200

    Add sunlight gem

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,10 @@
 require "csv"
+require 'rubygems'
+require 'sunlight'
+require 'open-uri'
+
+
+Sunlight::Base.api_key = "28ec1945859a4502a1477f6e42d8e36e"
 puts "EventManager Initialized!"
 
 
@@ -6,12 +12,24 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislators_by_zipcode(zipcode)
+  legislators = Sunlight::Legislator.all_in_zipcode(zipcode)
+
+  legislator_names = legislators.map do |legislator|
+    if legislator.nil?
+      no name
+    else
+      "#{legislator.first_name} #{legislator.last_name}"
+    end
+  end
+  legislator_names.join(", ")
+end
 
 contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+  puts "#{name} #{zipcode} #{legislators}"
 end
